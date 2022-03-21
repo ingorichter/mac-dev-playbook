521988576840f0781236792b3ca1010db0405a0e
Ingo Richter <ingo.richter+github@gmail.com>
2022-03-20 17:26:20 -0700

    Update config
    - improve boostrapping
    - remove obsolete apps
    - fix node install
    - install emacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,18 @@ set -euo pipefail
 
 # ensure xcode is working fine
 # download and install command line tools
-xcode-select --install
+XCodeDevToolsPath=$(xcode-select --print-path)
+
+${XCodeDevToolsPath}/usr/bin/pip3
+
+if [ $? eq 127 ]; then
+	xcode-select --install
+fi
 
 # install pip
 # sudo easy_install pip
-sudo pip3 install --upgrade pip
-sudo -H pip3 install ansible
+pip3 install --upgrade pip
+pip3 install ansible
 
 # check out ansible scripts into local directory
 mkdir -p ~/ansible-setup && cd ~/ansible-setup
--- run-ansible.sh
+++ run-ansible.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-ansible-galaxy install -r requirements.yml
-ansible-playbook -i inventory --ask-become-pass playbook.yml
+~/Library/Python/3.8/bin/ansible-galaxy install -r requirements.yml
+~/Library/Python/3.8/bin/ansible-playbook -i inventory --ask-become-pass playbook.yml
 
 #  Example to start at specific task
 # ansible-playbook -i inventory --ask-become-pass playbook.yml --start-at-task "Check if dot tool is already installed" -v

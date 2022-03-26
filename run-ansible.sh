d60128f0fba172233d91e4088b6574c6df43d20d
Ingo Richter <ingo.richter+github@gmail.com>
2022-03-25 21:13:33 -0700

    remove Python3 PATH from run-ansible.sh

--- run-ansible.sh
+++ run-ansible.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 # fix path
 export N_PREFIX="${HOME}/n"
-export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+# export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/bin:${PATH}"
 
 ~/Library/Python/3.8/bin/ansible-galaxy install -r requirements.yml
 ~/Library/Python/3.8/bin/ansible-playbook -i inventory --ask-become-pass playbook.yml

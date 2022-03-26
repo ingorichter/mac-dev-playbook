dcde5dadc8f3985b3ecc805fa11063441c2ea460
Ingo Richter <ingo.richter+github@gmail.com>
2022-03-25 21:00:44 -0700

    Update Config
    - ensure that the scripts work on Intel and M1
    - fix emacs installation
    - export Python3 in PATH for run-ansible.sh

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+# fix path
+export N_PREFIX="${HOME}/n"
+export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
 
 ~/Library/Python/3.8/bin/ansible-galaxy install -r requirements.yml
 ~/Library/Python/3.8/bin/ansible-playbook -i inventory --ask-become-pass playbook.yml
